fab9fa5d236be7e08e2090029eb09db5efa00b1a
Sumit Sijher <sumitsijher@gmail.com>
2020-06-15 01:24:39 -0700

    added animation record

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+const record_animation = false;
 
-const enable_interaction = true;
+const fps = 30;
+const total_frames = 200;
+const t_max = 5.2;
+const t_rate = t_max/total_frames;
+var frame = 0;
+var loop = 0;
+
+
+const enable_interaction = false;
 var get_mouse_pos = false;
 var get_touch_pos = false;
 
@@ -9,13 +18,12 @@ var mouse_y = window.innerHeight/2;
 var interaction_variables = [mouse_x, mouse_y];
 
 var t = 0;
-var t_rate = .006;
+//var t_rate = .006;
 
 var s = 120;
 var range = 2.5*s;
 
 var stop_animation = false;
-var fps = 60;
 var fpsInterval, startTime, now, then, elapsed;
 
 
@@ -28,8 +36,11 @@ startAnimating(fps);
 
 function draw() {
 
-    W = canvas.width = window.innerWidth;
-    H = canvas.height = window.innerHeight;
+    W = canvas.width = 600; //window.innerWidth;
+    H = canvas.height = 600; //window.innerHeight;
+
+    mouse_x = W/2*(1+Math.cos(2*Math.PI*t/t_max));
+    mouse_y = H/2*(1+Math.sin(2*Math.PI*t/t_max));
     
     ctx.fillStyle = `rgba(0,0,0,1)`;
     ctx.fillRect(0, 0, W, H);
@@ -43,18 +54,16 @@ function draw() {
             let y_pos = s/2 + j*s;
             let dist = Math.sqrt((x_pos - mouse_x)**2 + (y_pos - mouse_y)**2);
             let phase = 1 + 10/range*Math.min(dist, range);
-            tile(30, x_pos, y_pos, phase, t);
+            tile(30, x_pos, y_pos, phase, 10.5 + t%5.2);
         }
     }
-  
-    t += t_rate;
     
 }
 
 function tile(squares, x_pos, y_pos, phase, t) {
 
     for(let j=squares; j--; ) {
-        let hue = (10*t)%360;
+        let hue = 200;
         ctx.fillStyle= `hsla(${hue}, 100%, ${80*((j+1)%2)}%, 1)`;
         ctx.fillRect(x_pos+(z=Math.cos(t/100)*Math.cos(a=j-j%2)*(k=1.03**a)-(d=10*k*(j%2+5*Math.sin(j*t/phase)))/2),y_pos+z,1*d,d);
     }
@@ -83,7 +92,25 @@ function startAnimating(fps) {
      if (elapsed > fpsInterval) {
         then = now - (elapsed % fpsInterval);
      
-        draw();    
+        draw();  
+        
+        frame = (frame+1)%total_frames;
+        t = t_rate*frame;
+        
+
+        if(record_animation) {
+            if (frame + 1 === total_frames) {
+                loop += 1;
+            }
+    
+            if (loop === 1) { 
+                let frame_number = frame.toString().padStart(total_frames.toString().length, '0');
+                download('image_'+frame_number+'.png', canvas);
+            }
+    
+            if (loop === 2) { stop_animation = true }
+        }
+     
      }
 
     if(enable_interaction) {
@@ -132,8 +159,23 @@ function getTouchPosition(canvas, event) {
 
 function interaction(canvas, event, ...interaction_variables) {
 
-    mouse_x = event.clientX
-    mouse_y = event.clientY
+    mouse_x = event.clientX;
+    mouse_y = event.clientY;
 
 }
 
+function download(filename, canvas) {
+    dataURL = canvas.toDataURL();
+    var element = document.createElement('a');
+    element.setAttribute('href', dataURL);
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+  
+    console.log('Downloaded ' + filename);
+}
